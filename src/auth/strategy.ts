523b4a00bd3a21dbc9c5226a5b23ba4ae3ab4e92
ERLEY <erley.1701920254@ucaldas.edu.co>
2023-04-08 21:57:12 -0500

    creacion de estrategias de autorizacion beta

--- src/auth/strategy.ts
+++ src/auth/strategy.ts
@@ -3,8 +3,9 @@ import {inject} from '@loopback/core';
 import {HttpErrors, Request} from '@loopback/rest';
 import {UserProfile} from '@loopback/security';
 import parseBearerToken from 'parse-bearer-token';
+const fetch = require('node-fetch');
 //import {RolMenuRepository} from '../repositories';
-//import {SeguridadUsuarioService} from '../services';
+import {ConfiguracionSeguridad} from '../config/configuracion.seguridad';
 
 export class AuthStrategy implements AuthenticationStrategy {
   name: string = 'auth';
@@ -22,34 +23,37 @@ export class AuthStrategy implements AuthenticationStrategy {
    * @returns el perfin de usuario, undefined cuando no tiene permiso o un HttpError
    */
   async authenticate(request: Request): Promise<UserProfile | undefined> {
-    //console.log("ejecutando estrategia");
     let token = parseBearerToken(request);
     if (token) {
-      //let idRol = this.servicioSeguridad.obtenerRolDesdeToken(token);
       let idMenu: string = this.metadata[0].options![0];
       let accion: string = this.metadata[0].options![1];
-
       console.log(this.metadata);
 
-      //conectar con ms-seguridad
-      console.log("conectar con ms-seguridad");
-
-      /*let permiso = await this.repositorioRolMenu.findOne({
-        where: {
-          rolId: idRol,
-          menuId: idMenu
+      const datos = {token: token, idMenu: idMenu, accion: accion};
+      const urlValidarPermisos = `${ConfiguracionSeguridad.enlaceMicroservicioSeguridad}/validar-permisos`;
+      let res = undefined;
+      try {
+        await fetch(urlValidarPermisos, {
+          method: 'post',
+          body: JSON.stringify(datos),
+          headers: {'Content-Type': 'application/json'},
+        }).then((res: any) => res.json())
+          .then((json: any) => {
+            res = json;
+          });
+        if (res) {
+          let perfil: UserProfile = Object.assign({
+            permitido: "OK"
+          });
+          return perfil;
+        } else {
+          return undefined;
         }
-      });*/
-      let continuar: boolean = false;
-      if (continuar) {
-        let perfil: UserProfile = Object.assign({  //Si retorma aqui está bien
-          permitido: "ok"
-        });
-        return perfil;
-      } else {
-        return undefined; //Si retorna aqui es que algo ocurrio
+      } catch (e) {
+        throw new HttpErrors[401]("No se tiene permisos sobre la acción a ejecutar.");
       }
     }
-    throw new HttpErrors[401]("No es posible ejecutar la accion por falta de un token");
+    throw new HttpErrors[401]("No es posible ejecutar la acción por falta de un token.");
   }
 }
+
--- src/config/configuracion.seguridad.ts
+++ src/config/configuracion.seguridad.ts
@@ -1,8 +1,9 @@
 export namespace ConfiguracionSeguridad {
-  export const menuProductoId = "640f45d13b2c72633cc058f9";
+  export const menuProductoId = "642584f45edca461ec1fdbd1";
   export const listarAccion = "listar";
   export const guardarAccion = "guardar";
   export const editarAccion = "editar";
   export const eliminarAccion = "eliminar";
-  export const descargarAccion = "descargarr";
+  export const descargarAccion = "descargar";
+  export const enlaceMicroservicioSeguridad: string = "http://localhost:3000";
 }
